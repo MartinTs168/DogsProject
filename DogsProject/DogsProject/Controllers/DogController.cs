131a33ea27f88ec8d1bfd63b275a091d29e84edf
MartinTs168 <martin.tsenev@gmail.com>
2024-11-19 09:30:57 +0300

    added dog create and success actions and coresponding views

---
+++ DogsProject/DogsProject/Controllers/DogController.cs
@@ -0,0 +1,110 @@
+﻿using DogsProject.Data;
+using DogsProject.Infrastructure.Data.Entities;
+using DogsProject.Models.Dog;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsProject.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: DogController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DogCreateViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Dog dog = new Dog
+                {
+                    Name = model.Name,
+                    Age  = model.Age,
+                    Breed = model.Breed,
+                    Picture = model.Picture,
+                };
+
+                _context.Add(dog);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Success");
+            }
+
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        // GET: DogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
